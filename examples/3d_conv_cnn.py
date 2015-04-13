8a795a9149747029bac816bffb3acd4c128cc682
jvarley <jakevarley@gmail.com>
2015-04-13 18:17:17 -0400

    cleaning up example code

--- examples/3d_conv_cnn.py
+++ examples/3d_conv_cnn.py
@@ -13,7 +13,8 @@ patch_size = 32
 filter_size = 5
 nb_filter = 8
 
-nb_batches = 2000
+nb_train_batches = 100
+nb_test_batches = 20
 nb_classes = 3
 nb_epoch = 200
 
@@ -24,13 +25,6 @@ model.add(Flatten(nb_filter*14*14*14))
 model.add(Dense(nb_filter*14*14*14, nb_classes, init='normal'))
 model.add(Activation('softmax'))
 
-#Model without maxpooling
-# model = Sequential()
-# model.add(Convolution3D(nb_filter=4, stack_size=1, nb_row=filter_size, nb_col=filter_size, nb_depth=filter_size, border_mode='valid'))
-# model.add(Flatten(4*28*28*28))
-# model.add(Dense(4*28*28*28, nb_classes, init='normal'))
-# model.add(Activation('softmax'))
-
 # let's train the model using SGD + momentum (how original).
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
@@ -38,27 +32,24 @@ model.compile(loss='categorical_crossentropy', optimizer=sgd)
 dataset = geometric_3d_dataset.Geometric3DDataset(patch_size=patch_size,
                                                   task=geometric_3d_dataset.Geometric3DDataset.CLASSIFICATION_TASK,
                                                   centered=True)
-iterator = dataset.iterator(batch_size=batch_size,
-                            num_batches=nb_batches)
 
+for e in range(nb_epoch):
 
-for b in range(nb_batches):
-    # print '-'*40
-    # print 'mini batch', b
-    # print '-'*40
-    # print "Training..."
+    train_iterator = dataset.iterator(batch_size=batch_size,
+                                      num_batches=nb_test_batches)
 
-    X_batch, Y_batch = iterator.next()
-    loss = model.train(X_batch, Y_batch)
-    print loss
-        #progbar.add(X_batch.shape[0], values=[("train loss", loss)])
+    for b in range(nb_train_batches):
+        X_batch, Y_batch = train_iterator.next()
+        loss = model.train(X_batch, Y_batch)
+        print 'loss: ' + str(loss)
 
-    # print "Testing..."
-    # # test time!
-    # progbar = generic_utils.Progbar(X_test.shape[0])
-    # for X_batch, Y_batch in datagen.flow(X_test, Y_test):
-    #     score = model.test(X_batch, Y_batch)
-    #     progbar.add(X_batch.shape[0], values=[("test loss", score)])
+    test_iterator = dataset.iterator(batch_size=batch_size,
+                                     num_batches=nb_train_batches)
+
+    for b in range(nb_test_batches):
+        X_batch, Y_batch = test_iterator.next()
+        error = model.test(X_batch, Y_batch)
+        print 'error: ' + str(error)
 
 
 
